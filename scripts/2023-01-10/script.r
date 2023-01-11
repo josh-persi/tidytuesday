3cee36cb51c34824c83af7f3bf6c6d353313181d
Josh Persi <joshpersi@gmail.com>
2023-01-10 19:58:56 -0800

    Summarize observations by species and state

--- scripts/2023-01-10/script.r
+++ scripts/2023-01-10/script.r
@@ -5,10 +5,15 @@ p_load(
   dplyr,
   janitor,
   ggplot2,
+  leafgl,
+  leaflet,
   lubridate,
   purrr,
+  sf,
+  stringr,
   tidyr,
-  tidytuesdayR
+  tidytuesdayR,
+  tigris
 )
 
 # Load data
@@ -29,8 +34,85 @@ counts <- counts |>
   unite(date, year, month, day, sep = "-") |>
   mutate(date = as_date(date))
 
-# Plot the number of observations per day
+# Plot the number of observations per day. Some dates had a much higher number
+# of observations.
 counts |>
   count(date) |>
   ggplot(aes(date, n)) +
   geom_point()
+
+# The days with over 1000 observations were all saturdays
+counts |> 
+  count(date) |>
+  filter(n > 1000) |>
+  mutate(weekday = wday(date, label = TRUE), .before = everything()) |> 
+  count(weekday)
+
+# Observations are in the U.S. and Canada. Note the single point left of 
+# Australia
+leaflet() |> 
+  addTiles() |> 
+  addGlPoints(
+    data = st_as_sf(counts, coords = c("longitude", "latitude"), crs = 4326)
+    )
+
+# Download states geometries   
+states <- states()
+
+# Select the name and geometry columns and reproject to WGS84
+states <- states |> 
+  select(name = NAME, geometry) |> 
+  st_transform(crs = 4326)
+
+# States like Alaska and Hawaii are included 
+leaflet() |> 
+  addTiles() |> 
+  addPolygons(data = states, label = ~ name)
+
+# Filter out non-continuous U.S. states
+states <- filter(
+  states, 
+  !name %in% c(
+    "Alaska", 
+    "American Samoa",
+    "Commonwealth of the Northern Mariana Islands",
+    "Guam",
+    "Hawaii",
+    "Puerto Rico",
+    "United States Virgin Islands"
+    )
+  )
+
+# States like Alaska and Hawaii are included 
+leaflet() |> 
+  addTiles() |> 
+  addPolygons(data = states, label = ~ name)
+
+# Try to reformat the state codes and sum observations per state and species
+counts |> 
+  group_by(subnational1_code, species_code) |> 
+  summarize(how_many = sum(how_many)) |> 
+  filter(str_detect(subnational1_code, "US-")) |> 
+  mutate(
+    name = subnational1_code |> 
+      str_remove("US-") |> 
+      str_replace_all(state.abb, state.name)
+    )
+
+# Code the projection for the US National Atlas Equal Area projection
+epsg2163 <- str_c(
+  "+proj=laea",
+  "+lat_0=45",
+  "+lon_0=-100",
+  "+x_0=0",
+  "+y_0=0",
+  "+a=6370997",
+  "+b=6370997",
+  "+units=m",
+  "+no_defs", 
+  sep = " "
+)
+
+# An initial plot
+ggplot() +
+  geom_sf(data = st_transform(states, epsg2163))
